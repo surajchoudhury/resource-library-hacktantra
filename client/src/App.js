5d074486f52ebd3be1d18caa81fcab49e09d777d
sunny <sunnychoudhury007@gmail.com>
2020-02-15 12:39:15 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./stylesheets/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="reactlogo.gif" alt="" />
+      <p> Hey !!, Sunny Let's make something. </p>
+    </div>
+  );
+}
+
+export default App;

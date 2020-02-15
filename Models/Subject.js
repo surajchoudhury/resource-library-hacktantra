a2a1ce84ab3b36441e7c0f7e61944df18311d06d
Puneet <puneettiwari61@gmail.com>
2020-02-15 21:46:02 +0530

    subject model complete

---
+++ Models/Subject.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const subjectSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  modules: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Mentor'
+  }],
+  Image: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model('Subject', subjectSchema)

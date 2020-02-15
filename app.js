2d945e8663d853d566f35bed785edfd126479ea6
Puneet <puneettiwari61@gmail.com>
2020-02-15 20:36:05 +0530

    mongouriupdate

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 var usersRouter = require('./routes/v1/users');
 
 //mongoose connect to database
-mongoose.connect('mongodb://localhost/resources',{ useNewUrlParser: true, useUnifiedTopology: true },(err => {
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true },(err => {
   console.log('connected', err?false:true)
 }))
 

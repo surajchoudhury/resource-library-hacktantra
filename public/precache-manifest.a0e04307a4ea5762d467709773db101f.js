78422c5d2c459d407e1e3ac76223bfbb3cd4d459
sunny <sunnychoudhury007@gmail.com>
2020-02-28 16:03:38 +0530

    add favicon

---
+++ public/precache-manifest.a0e04307a4ea5762d467709773db101f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d7d507c6843fc5c761177ec54c7a20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98cc6f3965cebc329d4d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3cb41a0d718b4c0fbbae",
+    "url": "/static/css/main.b28a4d54.chunk.css"
+  },
+  {
+    "revision": "98cc6f3965cebc329d4d",
+    "url": "/static/js/2.7dd438d3.chunk.js"
+  },
+  {
+    "revision": "3cb41a0d718b4c0fbbae",
+    "url": "/static/js/main.09d656bf.chunk.js"
+  },
+  {
+    "revision": "c5393a09b8a7aa264c16",
+    "url": "/static/js/runtime-main.f0219fac.js"
+  },
+  {
+    "revision": "c620e3ef7b7e6c10cb649fc65250e76f",
+    "url": "/static/media/js.c620e3ef.jpg"
+  }
+]);
